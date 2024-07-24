96f909712976b00f97e07c505f90c0c0949b0807
Mihirjalihal <126067117+Mihirjalihal@users.noreply.github.com>
2024-07-24 09:41:57 +0530

    Add files via upload

---
+++ key.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+my_list = [10, 20, 30, 40, 50]
+key = 30
+
+if key in my_list:
+  print("Key found!")
+else:
+  print("Key not found.")
+
+
+# In[ ]:
+
+
+
+
